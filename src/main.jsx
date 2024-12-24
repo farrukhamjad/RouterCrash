dbc3f670f2fbc8f720c6b00bd99d3bd8341f43f2
Farrukh Amjad <farrukh.amjed@gmail.com>
2024-12-24 18:43:51 +0500

    Updated Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import User from './components/User/User'
-import Github, { githubInfoLoader } from './components/Github'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import User from './components/User/User';
+import Github, { githubInfoLoader } from './components/Github';
 
 const router = createBrowserRouter(
-  createRoutesFromElements (
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />}/>
-      <Route path='about' element={<About />}/>
-      <Route path='contact' element={<Contact />}/>
-      <Route loader={githubInfoLoader} path='github' element={<Github />}/>
-      <Route path='user/:userid' element={<User />}/>
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
+      <Route path="user/:userid" element={<User />} />
     </Route>
-  )
-)
+  ),
+  { basename: "/RouterCrash" }
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
